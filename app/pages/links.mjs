8d9139beefb574d93b701976de25b58b387a6a7c
macdonst <simon.macdonald@gmail.com>
2023-08-25 17:34:34 -0400

    Add PE delete button
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/pages/links.mjs
+++ app/pages/links.mjs
@@ -11,7 +11,7 @@ export default function Html ({ html, state }) {
   return html`<enhance-page-container>
   <main>
     <h1 class="mb1 font-semibold text3">Links page</h1>
-    ${links.map(item => `<article class="mb2">
+    ${links.map(item => `<article id="${item.key}" class="mb2">
 <div class="mb0">
   <p class="pb-2"><strong class="capitalize">text: </strong>${item?.text || ''}</p>
   <p class="pb-2"><strong class="capitalize">url: </strong>${item?.url || ''}</p>
@@ -20,9 +20,7 @@ export default function Html ({ html, state }) {
 <p class="mb-1">
   <enhance-link href="/links/${item.key}">Edit this link</enhance-link>
 </p>
-<form action="/links/${item.key}/delete" method="POST" class="mb-1">
-  <enhance-submit-button><span slot="label">Delete this link</span></enhance-submit-button>
-</form>
+<delete-button key="${item.key}"></delete-button>
 </article>`).join('\n')}
 <details class="mb0" ${Object.keys(problems).length ? 'open' : ''}>
     <summary>New link</summary>
