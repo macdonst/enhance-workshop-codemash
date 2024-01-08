513b8d777d1bfb97c5e312778174760bf60fdcd5
macdonst <simon.macdonald@gmail.com>
2024-01-08 11:45:58 -0500

    Module 2: start
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/browser/links-page.mjs
+++
@@ -1,5 +0,0 @@
-import DeleteButton from './delete-button.mjs'
-import LinkItem from './link-item.mjs'
-import SubmitButton from "./submit-button.mjs"
-
-export { DeleteButton, LinkItem, SubmitButton }
--- app/head.mjs
+++ app/head.mjs
@@ -1,73 +1,18 @@
-import titlesByPath from './lib/titles-by-path.mjs'
 import { getStyles }  from '@enhance/arc-plugin-styles'
 
 const { linkTag } = getStyles
 
-export default function Head(state) {
-  const { req, store } = state
-  const { path, session } = req
-
-  if (store.authorized === undefined) {
-    store.authorized = session.authorized || false
-  }
-  if (store.path === undefined) {
-    store.path = path
-  }
-  if (store.author === undefined) {
-    store.author = {
-      name: 'Axol Lotl',
-      title: 'Web Developer',
-      githubUsername: 'enhance-dev',
-    }
-  }
-
-  const title = titlesByPath[path] || ''
-
+export default function Head () {
   return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="og:type" content="website" />
-      <link rel="icon" href="/_public/favicon.svg">
-      <title>Axol Lotl: ${title}</title>
-      <meta name="description" content="Portfolio for Axol Lotl, Senior Developer" />
+      <title>Enhance Starter Project</title>
       ${linkTag()}
-
-      ${(req.path === '/resume') ? '<link rel="stylesheet" href="/_public/print-resume.css">' : ''}
-
-      <style>
-        @font-face {
-          font-family: "HK Grotesk";
-          font-weight: 300;
-          src: url("/_public/fonts/HKGrotesk-Light.woff2") format("woff2")
-        }
-
-        @font-face {
-          font-family: "HK Grotesk";
-          font-weight: 400;
-          src: url("/_public/fonts/HKGrotesk-Regular.woff2") format("woff2")
-        }
-
-        @font-face {
-          font-family: "HK Grotesk";
-          font-weight: 600;
-          src: url("/_public/fonts/HKGrotesk-SemiBold.woff2") format("woff2")
-        }
-
-        body {
-          color: var(--dark);
-          text-rendering: optimizeLegibility;
-        }
-
-        a {
-          text-decoration: underline;
-          text-decoration-thickness: 0.0625em;
-          text-underline-offset: 0.0625em;
-        }
-      </style>
+      <link rel="icon" href="/_public/favicon.svg">
+      <meta name="description" content="The HTML first full stack web framework.">
     </head>
-    <body class='font-sans'>
 `
 }
