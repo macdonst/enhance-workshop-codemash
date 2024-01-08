48e86b394692c214195823e773d9364faac2db33
macdonst <simon.macdonald@gmail.com>
2024-01-08 11:56:03 -0500

    Module 4 start
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

---
+++ app/elements/site-container.mjs
@@ -0,0 +1,14 @@
+// /app/elements/site-container.mjs
+export default function SiteContainer({ html }) {
+  return html`
+      <style>
+        :host {
+          display: block;
+          inline-size: var(--site-width);
+          max-inline-size: var(--site-max-width);
+          margin-inline: auto;
+        }
+      </style>
+      <slot></slot>
+    `
+}
