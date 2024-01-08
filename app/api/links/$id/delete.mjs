39fc8e7ec2345f794ea3cdaed7dce7abfbb69520
macdonst <simon.macdonald@gmail.com>
2024-01-08 14:13:31 -0500

    Module 8: Start
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

---
+++ app/api/links/$id/delete.mjs
@@ -0,0 +1,11 @@
+// /app/api/links/$id/delete
+import { deleteLink } from '../../../models/links.mjs'
+
+export async function post (req) {
+  const id = req.pathParameters?.id
+  let link = await deleteLink(id)
+  return {
+    json: { link },
+    location: '/links'
+  }
+}
