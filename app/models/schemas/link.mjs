bc2f43dd1bedd246ceabfcb897cc2dbf61dcf801
macdonst <simon.macdonald@gmail.com>
2024-01-08 13:59:36 -0500

    Module 7: start

---
+++ app/models/schemas/link.mjs
@@ -0,0 +1,22 @@
+// /app/models/schemas/link.mjs
+export const Link = {
+  "id": "Link",
+  "type": "object",
+  "properties": {
+    "text": {
+      "type": "string",
+      "minLength": 1,
+    },
+    "url": {
+      "type": "string",
+      "format": "url",
+    },
+    "published": {
+      "type": "boolean",
+    },
+    "key": {
+      "type": "string"
+    }
+  },
+  "required":["text", "url"],
+}
